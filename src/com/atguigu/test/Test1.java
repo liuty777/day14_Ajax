7398e817f07e1d98f4f95458616af08103a1e6ee
liuty777 <78328115+liuty777@users.noreply.github.com>
2021-02-02 20:48:10 +0800

    Update Test1.java

--- src/com/atguigu/test/Test1.java
+++ src/com/atguigu/test/Test1.java
@@ -3,6 +3,6 @@ package com.atguigu.test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("123798");
-        System.out.println("123321");
+        System.out.println("1233211231231231");
     }
 }
